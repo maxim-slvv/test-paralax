dbbb6f9045501e6eeda8e52b6312176f927e41a0
Maxim <panda.gtr007@gmail.com>
2022-11-22 13:38:43 +0600

    final parallax

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,22 +1,30 @@
 const parallax = document.querySelector('.parallax');
 const front = document.querySelector('.parallax__front');
 const back = document.querySelector('.parallax__back');
+const way = document.querySelector('.parallax__way');
 //сенса
-const sFront = 500;
-const sBack = 2000;
+const sFront = 800;
+const sBack = 1500;
+const sWay = 5000;
 
 parallax.addEventListener('mousemove', e => {
     const x = e.clientX;
     const y = e.clientY;
-
-    front.style.transform = `translate(
-        ${x / sFront}%,
-        ${y / sFront}%
-    )`;
-
-    back.style.transform = `translate(
-        ${x / sBack}%,
-        ${y / sBack}%
-    )`;
-
+    if(Math.max(
+        document.body.scrollWidth,
+        document.body.offsetWidth,
+        document.body.clientWidth) > 1300){ //если ширина экрана будет меньше 1300 то паралакс прекратится
+        front.style.transform = `translate(
+            ${x / sFront}%,
+            ${y / sFront}%
+        )`;
+        back.style.transform = `translate(
+            ${x / sBack}%,
+            ${y / sBack}%
+        )`;
+        way.style.transform = `translate(
+            ${x / sWay}%,
+            ${y / sWay}%
+        )`;
+    }
 });
